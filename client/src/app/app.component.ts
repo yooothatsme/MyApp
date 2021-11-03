db654990d1b09f0c57bff7df9aecb6a263720ca1
yooothatsme <yooothatsme@gmail.com>
2021-11-03 22:59:29 +0530

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users:any;
+
+  constructor(private http:HttpClient) {
+  }
+  ngOnInit(){
+  this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get("https://localhost:5001/api/user").subscribe(response=>{
+      this.users = response;
+    }, error=>{
+      console.log(error);
+    })
+    ;
+  }
+}
